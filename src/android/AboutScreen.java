a0eea3a47d74376aec51216d1691be238e1c2456
josephma93 <josephma93@gmail.com>
2014-12-22 01:40:02 -0600

    Android code working
    Some changes has been done in plugin.xml to be able to run the code.
    Android is working now.

--- src/android/AboutScreen.java
+++ src/android/AboutScreen.java
@@ -2,31 +2,51 @@ package com.github.josephma93.aboutscreen;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.util.DisplayMetrics;
+import java.lang.Math;
+
 
 /**
  * This class echoes a string called from JavaScript.
  */
-public class About screen extends CordovaPlugin {
+public class AboutScreen extends CordovaPlugin {
+
+    private static final String ACTION_GET_INFO = "getInfo";
+
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        if (action.equals(ACTION_GET_INFO)) {
+            this.getInfo(callbackContext);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
+    private void getInfo(CallbackContext callbackContext) {
+        DisplayMetrics dm = new DisplayMetrics();
+        (this.cordova.getActivity()).getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width  =     dm.widthPixels;
+        int height =     dm.heightPixels;
+        int dens   =     dm.densityDpi;
+        double wi  =     (double)width/(double)dens;
+        double hi  =     (double)height/(double)dens;
+        double x   =     Math.pow(wi,2);
+        double y   =     Math.pow(hi,2);
+        double screenDiagonal = Math.sqrt(x+y);
+        JSONObject info = new JSONObject();
+        try {
+            info.put("width",width);
+            info.put("height",height);
+            info.put("density",dens);
+            info.put("screenDiagonal",screenDiagonal);
+        } catch (JSONException e) {
+            callbackContext.error("Unexpected JSONException.");
+            e.printStackTrace();
         }
+        callbackContext.success(info);
     }
 }
--- www/aboutScreen.js
+++ www/aboutScreen.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "about screen", "coolMethod", [arg0]);
+exports.getInfo = function(success, error) {
+    exec(success, error, "AboutScreen", "getInfo", []);
 };
